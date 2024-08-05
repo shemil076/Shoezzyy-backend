bf55734ea246b96b997cde5a9f4b8b4c6d60fd08
Pramuditha Karunarathna <shemil@Pramudithas-MacBook-Pro.local>
2024-08-05 14:13:15 +0530

    Re implmented the image saving functionality

--- src/app.ts
+++ src/app.ts
@@ -4,38 +4,44 @@ import connectDB from './db';
 import createInitialDocuments from './initialDocuments';
 import orderRoutes from './routes/orderRoutes';
 import shoeRoutes from './routes/shoeRoutes';
-import adminRouters from './routes/adminAuth'
+import adminRouters from './routes/adminAuth';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
-// const port = process.env.PORT || 5000;
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 5000;
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const basename = path.basename(file.originalname, ext);
+    const newFilename = `${basename}-${Date.now()}${ext}`;
+    cb(null, newFilename);
+  },
+});
 
-// Multer setup for handling file uploads
-const upload = multer({ dest: 'uploads/' }); // You can configure the destination and storage options
+const upload = multer({ storage });
 
-// Middleware to parse JSON bodies with increased limit
+// Middleware to parse JSON bodies
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
+app.use('/uploads', express.static(path.resolve('uploads')));
+
+app.use('/brand/uploads', express.static(path.resolve('uploads')));
+
 // Routes
 app.use('/api/orders', orderRoutes);
-app.use('/api/shoes', upload.array('images'), shoeRoutes); // Apply multer middleware to shoeRoutes
-app.use('/api/shoes', shoeRoutes);
+app.use('/api/shoes', upload.array('images', 4), shoeRoutes); // Apply multer middleware to shoeRoutes
 app.use('/api/admin', adminRouters);
 
-
 // Start server and connect to DB
-// app.listen(port, async () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-//   await connectDB();
-//   await createInitialDocuments();
-// });
-
 app.listen(port, '0.0.0.0', async () => {
   console.log(`Server is running on http://localhost:${port}`);
   await connectDB();
